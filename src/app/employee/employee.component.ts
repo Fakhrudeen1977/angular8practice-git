6a07151e9ac273c0e1ae726d15733f1b0a4afe4d
Fakhrudeen1977 <fakhrudeen77@gmail.com>
2025-10-21 14:00:01 +0530

    Contact Preference added

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -10,14 +10,17 @@ from '@angular/forms';
 export class EmployeeComponent implements OnInit {
 
    employeeForm: FormGroup;
+   contactPreferenceOption:boolean=true;
+   selectedEmailorPhone:string;
    constructor(){
 
    }
     ngOnInit() {
         this.employeeForm = new FormGroup({
             fullName: new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]),
+            contactPreference:new FormControl('',Validators.required),
             email: new FormControl('',Validators.required),
-
+            phone: new FormControl('',Validators.required),            
             skills : new FormGroup({
             skillName: new FormControl("",Validators.required),
             experienceInYears: new FormControl("",[Validators.required, Validators.min(0), Validators.max(30)]),
@@ -43,11 +46,26 @@ export class EmployeeComponent implements OnInit {
      public get fullName(){
          return this.employeeForm.get('fullName');
      }
+
+     public onContactPreference(selectedValue:string){
+         this.contactPreferenceOption=false;
+         this.selectedEmailorPhone=selectedValue;
+         if(selectedValue==="Email"){
+             this.contactPreferenceOption=true;
+         }
+         else
+         this.contactPreferenceOption=false;
+     }
    
      public loadData():void{
+         this.selectedEmailorPhone="Email" ;
+                  
          this.employeeForm.setValue({
             fullName:"Fakhrudeen Ali Ahamed",
+            contactPreference:this.selectedEmailorPhone,
             email:"fakhrudeen77@gmail.com",
+            phone:8015409728,       
+           
             skills:{
                 skillName:"Java J2EE",
                 experienceInYears:"13",
